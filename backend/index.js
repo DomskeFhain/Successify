b6574cec204041bb58083ccb0434ea26b2ef2e02
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2025-04-30 16:53:46 +0200

    updated users login + post route

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,9 @@ const port = 9000;
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./database/successify.db");
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // Hilfsfunktionen
 
@@ -54,13 +53,13 @@ app.use(express.json());
 app.post("/users", (req, res) => {
   const { username, password } = req.body;
 
-  db.all(
+  db.get(
     "SELECT * FROM users WHERE username = ?",
     [username],
     async (err, rows) => {
       if (err) {
         res.status(500).send("Fehler in deiner Query Anfrage");
-      } else if (rows.length > 0) {
+      } else if (rows) {
         res.status(400).send("Benutzername existiert bereits");
       } else {
         const hashedPassword = await hashPassword(password);
@@ -85,26 +84,33 @@ app.post("/users", (req, res) => {
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
-  db.get("SELECT * FROM users WHERE username = ?", [username], async (err, rows) => {
-    if (err) {
-      return res.status(500).send("Fehler in deiner Query Anfrage");
-    } else if (rows.length === 0) {
-      return res.status(400).send("Benutzer nicht gefunden");
-    } else {
-      const match = await verifyPassword(password, rows.password);
-      if (!match) {
-        return res.status(400).send("Passwortd ist falsch");
-
+  db.get(
+    "SELECT * FROM users WHERE username = ?",
+    [username],
+    async (err, rows) => {
+      if (err) {
+        return res.status(500).send("Fehler in deiner Query Anfrage");
+      } else if (!rows) {
+        return res.status(400).send("Benutzer nicht gefunden");
+      } else {
+        console.log(rows);
+        const match = await verifyPassword(password, rows.password);
+        if (!match) {
+          return res.status(400).send("Passwortd ist falsch");
+        }
+        const token = jwt.sign(
+          { id: rows.id, username: rows.username },
+          process.env.JWT_SECRET,
+          { expiresIn: "1h" }
+        );
+        return res.status(200).json({
+          message: "Login erfolgreich",
+          token: token,
+        });
       }
-      const token = jwt.sign({ id: rows.id, username: rows.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
-      return res.status(200).json({
-        message: 'Login erfolgreich',
-        token: token,
-      })
     }
-  })
-
-})
+  );
+});
 
 app.listen(port, () => {
   console.log(`Server läuft auf Port ${port}`);
