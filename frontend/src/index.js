bdd558bc4d46ff5b8a11da456f4fe71764448248
KAYT-Techstarter <marvin.young@tn.techstarter.de>
2025-05-02 11:37:31 +0200

    Add Navbar & Home
    Navbar Component Created, Home Page  Created, Routing implemented

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Routes , Route} from 'react-router-dom'
+import Home from './pages/home/home';
+import Navbar from './components/Navbar/navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />  
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
