484b705fc1558bba9ac7c2cf18c057920e26ab8c
Dominik Schelske (Domske) <190346505+DomskeFhain@users.noreply.github.com>
2025-05-06 14:54:30 +0200

    -

--- backend/index.js
+++ backend/index.js
@@ -370,6 +370,109 @@ app.get("/shoppinglist", auth, (req, res) => {
   }
 });
 
+// To-Do
+
+//LIST ROUTE
+
+app.get("/todolist", auth, (req, res) => {
+  db.all("SELECT * FROM todoList WHERE userID = ?", [req.user.id], (err, rows) => {
+      if (err) {
+          return res.status(500).send("Error in Query Request");
+      } else {
+          return res.status(200).json(rows);
+      }
+  });
+});
+
+app.post("/todolist", auth, (req, res) => {
+  db.run("INSERT INTO todoList (listName, userId) VALUES (?, ?)", [req.body.listName, req.user.id], (err) => {
+      
+  
+  
+  if (err) {
+      return res.status(500).send("ERROR!");
+  } else {
+      return res.status(200).json([{message: "List created successfully",}]);
+  }
+})});
+
+app.put("/todolist/:listId", auth, (req, res) => {
+  const id = req.params.listId;
+  const listName = req.body.listName;
+  db.run("UPDATE todoList SET listName = ? WHERE listId = ?", [listName, id], (err) => {
+      if (err) {
+          return res.status(500).send("ERROR!");
+      } else {
+          return res.status(200).json([{message: "List updated successfully",}]);
+      }
+  })
+})
+
+app.delete("/todolist/:listId", auth, (req, res) => {
+  const id = req.params.listId;
+  db.run("DELETE FROM todoList WHERE listId = ?", [id], (err) => {
+      if (err) {
+          return res.status(500).send("ERROR!");
+      } else {
+          return res.status(200).json([{message: "List deleted successfully",}]);
+      }
+  })
+})
+
+// TASK ROUTE
+
+app.get("/tasks/:listId", auth, (req, res) => {
+  const id = req.params.listId;
+  db.all("SELECT * FROM tasksList WHERE listID = ?", [id], (err, rows) => {
+      if (err) {
+          return res.status(500).send("Error in Query Request");
+      } else {
+          return res.status(200).json(rows);
+      }
+  });
+});
+
+app.post("/tasks/:listId", auth, (req, res) => {
+  const id = req.params.listId;
+  const taskName = req.body.taskName;
+  db.run("INSERT INTO tasksList (taskName, listID) VALUES (?, ?)", [taskName, id
+  ], (err) => {
+      
+  
+  if (err) {
+      return res.status(500).send("ERROR!");
+  } else {
+      return res.status(200).json([{message: "Task created successfully",}]);
+  }}
+)});
+
+app.put("/tasks/:taskId", auth, (req, res) => {
+  const id = req.params.taskId;
+  const taskName = req.body.taskName;
+  db.run("UPDATE tasksList SET taskName = ? WHERE listId = ?", [taskName, id], (err) => {
+      if (err) {
+          return res.status(500).send("ERROR!");
+      } else {
+          return res.status(200).json([{message: "Task updated successfully",}]);
+      }
+  });
+})
+
+app.delete("/tasks/:taskId", auth, (req, res) => {
+  const id = req.params.taskId;
+  console.log(id);
+  db.run("DELETE FROM tasksList WHERE taskId = ?", [id], (err) => {
+      if (err) {
+          return res.status(500).send("ERROR!");
+      } else {
+          return res.status(200).json([{message: "Task deleted successfully",}]);
+      }
+      
+  });
+})
+
+// To-Do End
+
 app.listen(port, () => {
   console.log(`Server läuft auf Port ${port}`);
 });
--- backend/todo.js
+++ backend/todo.js
@@ -1,6 +1,8 @@
 // To-Do
 
-app.get("/todo", auth, (req, res) => {
+//LIST ROUTE
+
+app.get("/todolist", auth, (req, res) => {
     db.all("SELECT * FROM todoList WHERE userID = ?", [req.user.id], (err, rows) => {
         if (err) {
             return res.status(500).send("Fehler in deiner Query Anfrage");
@@ -10,7 +12,7 @@ app.get("/todo", auth, (req, res) => {
     });
   });
   
-  app.post("/todo", auth, (req, res) => {
+  app.post("/todolist", auth, (req, res) => {
     db.run("INSERT INTO todoList (listName, userId) VALUES (?, ?)", [req.body.listName, req.user.id], (err) => {
         
     
@@ -21,6 +23,20 @@ app.get("/todo", auth, (req, res) => {
         return res.status(200).json([{message: "Liste erfolgreich erstellt",}]);
     }
   })});
+  
+  app.put("/todolist/:listId", auth, (req, res) => {
+    const id = req.params.listId;
+    const listName = req.body.listName;
+    db.run("UPDATE todoList SET listName = ? WHERE listId = ?", [listName, id], (err) => {
+        if (err) {
+            return res.status(500).send("ERROR!");
+        } else {
+            return res.status(200).json([{message: "Liste erfolgreich bearbeitet",}]);
+        }
+    })
+  })
+  
+  // TASK ROUTE
   
   app.get("/tasks/:listId", auth, (req, res) => {
     const id = req.params.listId;
@@ -47,5 +63,17 @@ app.get("/todo", auth, (req, res) => {
     }}
   )});
   
-  // To-Do
+  app.put("/tasks/:taskId", auth, (req, res) => {
+    const id = req.params.taskId;
+    const taskName = req.body.taskName;
+    db.run("UPDATE tasksList SET taskName = ? WHERE listId = ?", [taskName, id], (err) => {
+        if (err) {
+            return res.status(500).send("ERROR!");
+        } else {
+            return res.status(200).json([{message: "Task erfolgreich geupdated",}]);
+        }
+    });
+  })
+  
+  // To-Do End
   
